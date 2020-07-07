1e19729b3e23ffbee0698349f35f0805a41ae1de
ddidonato <dannydd@me.com>
2020-07-07 14:12:51 -0400

    add style to set default margins

--- cuesubmit/cuesubmit/ui/Style.py
+++ cuesubmit/cuesubmit/ui/Style.py
@@ -16,3 +16,19 @@ def setFont(font):
 def init():
     """initialize the global style settings"""
     setFont(DEFAULT_FONT)
+
+
+class NoMarginsStyle(QtWidgets.QProxyStyle):
+    def pixelMetric(self, QStyle_PixelMetric, QStyleOption_option=None, QWidget_widget=None):
+        if QStyle_PixelMetric == QtWidgets.QStyle.PM_LayoutLeftMargin:
+            return 0
+        if QStyle_PixelMetric == QtWidgets.QStyle.PM_LayoutRightMargin:
+            return 0
+        if QStyle_PixelMetric == QtWidgets.QStyle.PM_LayoutTopMargin:
+            return 0
+        if QStyle_PixelMetric == QtWidgets.QStyle.PM_LayoutBottomMargin:
+            return 0
+        else:
+            return QtWidgets.QCommonStyle.pixelMetric(self, QStyle_PixelMetric, QStyleOption_option=QStyleOption_option,
+                                                           QWidget_widget=QWidget_widget)
+
