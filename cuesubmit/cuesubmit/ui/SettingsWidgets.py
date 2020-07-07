c54d0924ee7b535d11f4be2b066b6d4ca3898f45
ddidonato <dannydd@me.com>
2020-07-07 14:44:19 -0400

    settings widget changes (ui fixes, default values, file browser)

--- cuesubmit/cuesubmit/ui/SettingsWidgets.py
+++ cuesubmit/cuesubmit/ui/SettingsWidgets.py
@@ -18,7 +18,6 @@ class BaseSettingsWidget(QtWidgets.QWidget):
         super(BaseSettingsWidget, self).__init__(parent)
         self.mainLayout = QtWidgets.QVBoxLayout()
         self.setLayout(self.mainLayout)
-        self.mainLayout.setContentsMargins(0, 0, 0, 0)
 
     def getCommandData(self):
         """Override this method to return a dictionary of the settings data."""
@@ -32,27 +31,31 @@ class BaseSettingsWidget(QtWidgets.QWidget):
 class InMayaSettings(BaseSettingsWidget):
     """Settings widget to be used when launching from within Maya."""
 
-    def __init__(self, cameras=None, filename=None, parent=None, *args, **kwargs):
+    def __init__(self, cameras=[], filename='', project='', parent=None, *args, **kwargs):
         super(InMayaSettings, self).__init__(parent=parent)
-        self.mayaFileInput = Widgets.CueLabelLineEdit('Maya File:', filename)
+        self._filename = filename
+        self._project = project
+        self.mayaFileInput = Widgets.CueFileBrowserLineEdit('Maya File:', defaultText=self._filename, fileTypes=['ma', 'mb'])
+        self.mayaProjectInput = Widgets.CueFileBrowserLineEdit('Maya Project:', defaultText=self._project, selectFolder=True)
         self.cameraSelector = Widgets.CueSelectPulldown('Render Cameras', options=cameras)
         self.selectorLayout = QtWidgets.QHBoxLayout()
-        self.setupUi()
+        super(InMayaSettings, self).__init__(parent=parent)
 
     def setupUi(self):
-        self.mainLayout.addWidget(self.mayaFileInput)
+        super(InMayaSettings, self).setupUi()
         self.selectorLayout.addWidget(self.cameraSelector)
-        self.selectorLayout.addSpacerItem(Widgets.CueSpacerItem(Widgets.SpacerTypes.HORIZONTAL))
         self.mainLayout.addLayout(self.selectorLayout)
 
     def setCommandData(self, commandData):
-        self.mayaFileInput.setText(commandData.get('mayaFile', ''))
+        self.mayaFileInput.setText(commandData.get('mayaFile', self._filename))
+        self.mayaProjectInput.setText(commandData.get('mayaProject', self._project))
         self.cameraSelector.setChecked(commandData.get('camera', '').split(','))
 
     def getCommandData(self):
         return {
             'mayaFile': self.mayaFileInput.text(),
-            'camera': self.cameraSelector.text()
+            'mayaProject': self.mayaProjectInput.text(),
+            'camera': self.cameraSelector.text(),
         }
 
 
@@ -61,22 +64,27 @@ class BaseMayaSettings(BaseSettingsWidget):
 
     def __init__(self, parent=None, *args, **kwargs):
         super(BaseMayaSettings, self).__init__(parent=parent)
-        self.mayaFileInput = Widgets.CueLabelLineEdit('Maya File:')
+        self.mayaFileInput = Widgets.CueFileBrowserLineEdit('Maya File:', fileTypes=['ma', 'mb'])
+        self.mayaProjectInput = Widgets.CueFileBrowserLineEdit('Maya Project:', selectFolder=True)
         self.setupUi()
         self.setupConnections()
 
     def setupUi(self):
         self.mainLayout.addWidget(self.mayaFileInput)
+        self.mainLayout.addWidget(self.mayaProjectInput)
 
     def setupConnections(self):
         self.mayaFileInput.lineEdit.textChanged.connect(self.dataChanged.emit)
+        self.mayaProjectInput.lineEdit.textChanged.connect(self.dataChanged.emit)
 
     def setCommandData(self, commandData):
         self.mayaFileInput.setText(commandData.get('mayaFile', ''))
+        self.mayaProjectInput.setText(commandData.get('mayaProject', ''))
 
     def getCommandData(self):
         return {
             'mayaFile': self.mayaFileInput.text(),
+            'mayaProject': self.mayaProjectInput.text(),
         }
 
 
@@ -85,7 +93,7 @@ class InNukeSettings(BaseSettingsWidget):
 
     def __init__(self, writeNodes=None, filename=None, parent=None, *args, **kwargs):
         super(InNukeSettings, self).__init__(parent=parent)
-        self.fileInput = Widgets.CueLabelLineEdit('Nuke File:', filename)
+        self.fileInput = Widgets.CueFileBrowserLineEdit('Nuke File:', defaultText=filename, fileTypes=['nk'])
         self.writeNodeSelector = Widgets.CueSelectPulldown('Write Nodes:', emptyText='[All]',
                                                            options=writeNodes)
         self.selectorLayout = QtWidgets.QHBoxLayout()
@@ -94,7 +102,6 @@ class InNukeSettings(BaseSettingsWidget):
     def setupUi(self):
         self.mainLayout.addWidget(self.fileInput)
         self.selectorLayout.addWidget(self.writeNodeSelector)
-        self.selectorLayout.addSpacerItem(Widgets.CueSpacerItem(Widgets.SpacerTypes.HORIZONTAL))
         self.mainLayout.addLayout(self.selectorLayout)
 
     def setCommandData(self, commandData):
@@ -113,7 +120,7 @@ class BaseNukeSettings(BaseSettingsWidget):
 
     def __init__(self, parent=None, *args, **kwargs):
         super(BaseNukeSettings, self).__init__(parent=parent)
-        self.fileInput = Widgets.CueLabelLineEdit('Nuke File:')
+        self.fileInput = Widgets.CueFileBrowserLineEdit('Nuke File:', fileTypes=['nk'])
         self.setupUi()
         self.setupConnections()
 
@@ -161,33 +168,38 @@ class BaseBlenderSettings(BaseSettingsWidget):
 
     def __init__(self, parent=None, *args, **kwargs):
         super(BaseBlenderSettings, self).__init__(parent=parent)
-        self.fileInput = Widgets.CueLabelLineEdit('Blender File:')
-        self.outputPath = Widgets.CueLabelLineEdit(
+        self.fileInput = Widgets.CueFileBrowserLineEdit('Blender File:', fileTypes=['blend'])
+        self.outputPath = Widgets.CueFileBrowserLineEdit(
             'Output Path (Optional):',
             tooltip='Optionally set the rendered output format. '
                     'See the "-o" flag of {} for more info.'.format(
-                            Constants.BLENDER_OUTPUT_OPTIONS_URL))
+                            Constants.BLENDER_OUTPUT_OPTIONS_URL),
+            selectFolder=True
+        )
         self.outputSelector = Widgets.CueSelectPulldown(
             'Output Format', options=Constants.BLENDER_FORMATS, multiselect=False)
         self.outputLayout = QtWidgets.QHBoxLayout()
         self.setupUi()
         self.setupConnections()
-    
+
     def setupUi(self):
         self.mainLayout.addWidget(self.fileInput)
-        self.mainLayout.addLayout(self.outputLayout)
-        self.outputLayout.addWidget(self.outputPath)
+        self.outputSelector.toolButton.setFixedWidth(120)
         self.outputLayout.addWidget(self.outputSelector)
-    
+        self.outputLayout.addStretch(1)
+        self.outputLayout.setStretchFactor(self.outputSelector, 0)
+        self.mainLayout.addLayout(self.outputLayout)
+        self.mainLayout.addWidget(self.outputPath)
+
     def setupConnections(self):
         self.fileInput.lineEdit.textChanged.connect(self.dataChanged.emit)
         self.outputPath.lineEdit.textChanged.connect(self.dataChanged.emit)
-      
+
     def setCommandData(self, commandData):
         self.fileInput.setText(commandData.get('nukeFile', ''))
         self.outputPath.setText(commandData.get('outputPath', ''))
         self.outputSelector.setChecked(commandData.get('outputFormat', ''))
-    
+
     def getCommandData(self):
         return {
             'blenderFile': self.fileInput.text(),
